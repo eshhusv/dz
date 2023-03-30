8df08593ed8a08fa9040840371869c877c353a33
Ilya <dmitret-i2003@mail.ru>
2023-03-30 17:51:09 +0200

    Add project files.

---
+++ dz/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ISP22_2;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISP22_2
+{
+    public partial class MainWindow : Window
+    {
+        private OurList<int> ourList = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(txtLab8.Text);
+            ourList.Add(n);
+            list8.Items.Add(n);
+            txtLab8.Clear();
+<<<<<<< HEAD
+            updateList8();
+=======
+            upadteList8();
+>>>>>>> 61ec8eb883e200c4c6d1fcf4a7015c9cf7568a15
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+<<<<<<< HEAD
+            int n = ourList.IndexOf(10);
+            ourList.Insert(12, n);
+            updateList8();
+        }
+        private void updateList8()
+=======
+
+        }
+        private void upadteList8()
+>>>>>>> 61ec8eb883e200c4c6d1fcf4a7015c9cf7568a15
+        {
+            list8.Items.Clear();
+            foreach (int i in ourList)
+            {
+                list8.Items.Add(i);
+            }
+        }
+    }
+}
---
+++ dz/OurList.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP22_2
+{
+    internal class OurList<T> : IEnumerable<T>
+    {
+        Node<T> head; // головной/первый элемент
+        Node<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        // добавление элемента
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+                tail.Next = node;
+            tail = node;
+
+            count++;
+        }
+        // удаление элемента
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        // убираем узел current, теперь previous ссылается не на current, а на current.Next
+                        previous.Next = current.Next;
+
+                        // Если current.Next не установлен, значит узел последний,
+                        // изменяем переменную tail
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        // если удаляется первый элемент
+                        // переустанавливаем значение head
+                        head = head.Next;
+
+                        // если после удаления список пуст, сбрасываем tail
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+        // очистка списка
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+        // содержит ли список элемент
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+        // добвление в начало
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+        public void Insert(T data, int n)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> current = head;
+            Node<T> prev = null;
+            Node<T> next = null;
+            int i = 0;
+            while (current != null)
+            {
+                if (i == n + 1)
+                {
+                    next = current;
+                    prev.Next = node;
+                    node.Next = next;
+                    break;
+                }
+                else if (i == n)
+                {
+                    prev = current;
+                }
+                current = current.Next;
+                i++;
+            }
+        }
+        public int IndexOf(T data)
+        {
+            Node<T> current = head;
+            int i = 0;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return i;
+                }
+                current = current.Next;
+                i++;
+            }
+            return -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+    }
+}
