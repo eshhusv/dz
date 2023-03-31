1d61eff0dc130a6c0caeec867bbcbb5b7e13bc14
eshhusv <ak7527203@gmail.com>
2023-03-31 20:52:58 +0200

    lol

--- dz/MainWindow.xaml.cs
+++ dz/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using ISP22_2;
+﻿using dz;
+using ISP22_2;
+using lab678;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +23,7 @@ namespace ISP22_2
     public partial class MainWindow : Window
     {
         private OurList<int> ourList = new();
+        private NodeStack<Product> stack = new();
         public MainWindow()
         {
             InitializeComponent();
@@ -27,24 +31,37 @@ namespace ISP22_2
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int n = int.Parse(txtLab8.Text);
-            ourList.Add(n);
-            list8.Items.Add(n);
-            txtLab8.Clear();
-            updateList8();
+            try
+            {
+                int n = int.Parse(txtLab8.Text);
+                ourList.Add(n);
+                list8.Items.Add(n);
+                txtLab8.Clear();
+                updateList8();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            foreach(int i in ourList)
+            try
             {
-                if(Math.Abs(i) < 2)
+                foreach (int i in ourList)
                 {
-                    ourList.Insert(15, i - 1);
-                    updateList8();
+                    if (Math.Abs(i) < 2)
+                    {
+                        ourList.Insert(15, i - 1);
+                        updateList8();
+                    }
                 }
             }
-            
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private void updateList8()
 
@@ -53,6 +70,58 @@ namespace ISP22_2
             foreach (int i in ourList)
             {
                 list8.Items.Add(i);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (textBox6.Text != null)
+                {
+                    Random r = new();
+                    int numOfElements = int.Parse(textBox6.Text);
+                    List<double> array6 = new();
+                    for (int i = 0; i < numOfElements; i++)
+                    {
+                        double tempNum = r.NextDouble();
+                        array6.Add(tempNum);
+                    }
+                    foreach (double item in array6)
+                    {
+                        textBlock6.Text += item.ToString("F2") + " ";
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Product product = new(productName.Text, double.Parse(productPrice.Text));
+                stack.Push(product);
+                TreeViewItem item = new();
+                item.Tag = "Number " + stack.Count;
+                item.Header = "Number " + stack.Count;
+                item.Items.Add(product.Name);
+                item.Items.Add(product.Price);
+                treeList.Items.Add(item);
+                productName.Clear();
+                productPrice.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
